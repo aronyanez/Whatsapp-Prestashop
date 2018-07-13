27fe3dd75c7aeb07af710dcfc74d8cca5e41df2f
Unknown <aronyanez@outlook.com>
2018-07-13 17:57:15 -0500

    Added spanish translation

--- translations/es.php
+++ translations/es.php
@@ -1,16 +1,16 @@
 <?php
+
 global $_MODULE;
 $_MODULE = array();
-$_MODULE['<{markwhatsapp}prestashop>markwhatsapp_47e284072c9a8b6343051999da78ba53'] = 'Contacto vía Whatsapp';
-$_MODULE['<{markwhatsapp}prestashop>markwhatsapp_57125349876de1f343258a54ebd4c935'] = 'Agregar contacto vía Whatsapp';
-$_MODULE['<{markwhatsapp}prestashop>markwhatsapp_876f23178c29dc2552c0b48bf23cd9bd'] = '¿Quieres desinstalar?';
-$_MODULE['<{markwhatsapp}prestashop>markwhatsapp_d5d5da0b761e3dabafb2adc9b35e0e95'] = 'Quiero información';
-$_MODULE['<{markwhatsapp}prestashop>markwhatsapp_fe5d926454b6a8144efce13a44d019ba'] = 'Configuración no válida';
-$_MODULE['<{markwhatsapp}prestashop>markwhatsapp_c888438d14855d7d96a2724ee9c306bd'] = 'Ajustes actualizados';
-$_MODULE['<{markwhatsapp}prestashop>markwhatsapp_f4f70727dc34561dfde1a3c529b6205c'] = 'Ajustes';
-$_MODULE['<{markwhatsapp}prestashop>markwhatsapp_1e4dbc7eaa78468a3bc1448a3d68d906'] = 'Numero de Móvil';
-$_MODULE['<{markwhatsapp}prestashop>markwhatsapp_521daf39e372791baf8221f9ff0727e1'] = 'Tu número de móvil';
-$_MODULE['<{markwhatsapp}prestashop>markwhatsapp_4c2a8fe7eaf24721cc7a9f0175115bd4'] = 'Mensaje';
-$_MODULE['<{markwhatsapp}prestashop>markwhatsapp_dc0dd5ec4394a239a1671db90f0c3821'] = 'Mensaje inicial';
-$_MODULE['<{markwhatsapp}prestashop>markwhatsapp_c9cc8cce247e49bae79f15173ce97354'] = 'Guardar';
-$_MODULE['<{markwhatsapp}prestashop>markwhatsapp_630f6dc397fe74e52d5189e2c80f282b'] = 'Regresar a Módulos';
+$_MODULE['<{markcontactwhatsapp}prestashop>markcontactwhatsapp_47e284072c9a8b6343051999da78ba53'] = 'Contacto vía Whatsapp';
+$_MODULE['<{markcontactwhatsapp}prestashop>markcontactwhatsapp_57125349876de1f343258a54ebd4c935'] = 'Agregar Contacto vía Whatsapp';
+$_MODULE['<{markcontactwhatsapp}prestashop>markcontactwhatsapp_876f23178c29dc2552c0b48bf23cd9bd'] = '¿Estas seguro que quieres desinstalar ?';
+$_MODULE['<{markcontactwhatsapp}prestashop>markcontactwhatsapp_d5d5da0b761e3dabafb2adc9b35e0e95'] = 'Quiero información';
+$_MODULE['<{markcontactwhatsapp}prestashop>markcontactwhatsapp_c888438d14855d7d96a2724ee9c306bd'] = 'Ajustes actualizados';
+$_MODULE['<{markcontactwhatsapp}prestashop>markcontactwhatsapp_f4f70727dc34561dfde1a3c529b6205c'] = 'Ajustes';
+$_MODULE['<{markcontactwhatsapp}prestashop>markcontactwhatsapp_1e4dbc7eaa78468a3bc1448a3d68d906'] = 'Número Teléfonico';
+$_MODULE['<{markcontactwhatsapp}prestashop>markcontactwhatsapp_521daf39e372791baf8221f9ff0727e1'] = 'Tu número teléfonico';
+$_MODULE['<{markcontactwhatsapp}prestashop>markcontactwhatsapp_4c2a8fe7eaf24721cc7a9f0175115bd4'] = 'Mensaje';
+$_MODULE['<{markcontactwhatsapp}prestashop>markcontactwhatsapp_dc0dd5ec4394a239a1671db90f0c3821'] = 'Tu mensaje inicial';
+$_MODULE['<{markcontactwhatsapp}prestashop>markcontactwhatsapp_c9cc8cce247e49bae79f15173ce97354'] = 'Guardar';
+$_MODULE['<{markcontactwhatsapp}prestashop>markcontactwhatsapp_630f6dc397fe74e52d5189e2c80f282b'] = 'Regresar a la lista';
